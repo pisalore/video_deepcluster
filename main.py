d3fc0152aa18f568ab6428d5344a2a2f4c03c92e
pisalore <lorenzo.pisaneschi@gmail.com>
2021-03-04 13:54:42 +0100

    bug fix

--- main.py
+++ main.py
@@ -303,28 +303,29 @@ def compute_features(dataloader, model, N):
     model.eval()
     # discard the label information in the dataloader; load the sample image.
     for i, sample in enumerate(dataloader):
-        if i % 2:
+        if not i % 2:
             input_var = torch.autograd.Variable(sample['image'].cuda(), volatile=True)
             aux = model(input_var).data.cpu().numpy()
 
-        if i == 0:
-            features = np.zeros((N, aux.shape[1]), dtype='float32')
+            if i == 0:
+                features = np.zeros((N, aux.shape[1]), dtype='float32')
 
-        aux = aux.astype('float32')
-        if i < len(dataloader) - 1:
-            features[i * args.batch: (i + 1) * args.batch] = aux
-        else:
-            # special treatment for final batch
-            features[i * args.batch:] = aux
+            aux = aux.astype('float32')
 
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
+            if i < len(dataloader) - 1:
+                features[i * args.batch: (i + 1) * args.batch] = aux
+            else:
+                # special treatment for final batch
+                features[i * args.batch:] = aux
 
-        if args.verbose and (i % 200) == 0:
-            print('{0} / {1}\t'
-                  'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})'
-                  .format(i, len(dataloader), batch_time=batch_time))
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if args.verbose and (i % 200) == 0:
+                print('{0} / {1}\t'
+                      'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})'
+                      .format(i, len(dataloader), batch_time=batch_time))
     return features
 
 
