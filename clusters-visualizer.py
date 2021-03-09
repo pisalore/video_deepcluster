8223aecb21c69858ab4db778cf981f6f469be637
pisalore <lorenzo.pisaneschi@gmail.com>
2021-03-09 17:42:31 +0100

    implemented cluster visualizer

--- clusters-visualizer.py
+++ clusters-visualizer.py
@@ -1,9 +1,9 @@
 import argparse
 import pickle
 import os
+import random
 import torchvision.datasets as datasets
 import matplotlib.pyplot as plt
-from skimage import io
 
 
 def parse_args():
@@ -21,22 +21,35 @@ def clusters_reader(clusters):
         return pickle.load(f)
 
 
-def save_cluster_imgs(dataset, clusters, save_dir):
+def save_cluster_images(cluster,  cl_name):
+    if len(cluster) == 9:
+        img_dim = 3
+        cols, rows = img_dim, img_dim
+        fig = plt.figure(figsize=(8, 8))
+        for i in range(cols * rows):
+            img = plt.imread(cluster[i])
+            fig.add_subplot(rows, cols, i + 1)
+            plt.imshow(img)
+        plt.savefig(cl_name)
+
+
+def save_clusters(dataset, clusters, save_dir):
     for idx, c in enumerate(clusters):
         with open(os.path.join(save_dir, str(idx) + "_cluster.txt"), "w") as output:
             output.write("Cluster " + str(idx) + '\n')
+            imgs_list = []
             for im in c:
                 output.write(str(dataset[im][0] + '\n'))
+                imgs_list.append(str(dataset[im][0]))
+            if len(imgs_list) >= 9:
+                save_cluster_images(random.sample(imgs_list, 9), os.path.join(save_dir, 'cluster_' + str(idx) + ".png"))
 
 
 def main(args):
     clusters = clusters_reader(args.clusters)
     dataset = datasets.ImageFolder(args.dataset)
     print("Number of images inside dataset:", len(dataset))
-    save_cluster_imgs(dataset.imgs, clusters[0], args.save_dir)
-    # im2 = io.imread(dataset.imgs[0][0])
-    # plt.imshow(im2)
-    # plt.show()
+    save_clusters(dataset.imgs, clusters[-1], args.save_dir)
 
 
 if __name__ == '__main__':
