9a238129ec1cd7a5eab6b281d8ab300f8ebdb7b6
pisalore <lorenzo.pisaneschi@gmail.com>
2021-03-23 18:10:20 +0100

    Code reworking, phase 1: clean data. Added labels handling.

--- clean_data.py
+++ clean_data.py
@@ -8,6 +8,7 @@ import torchvision.transforms as transforms
 
 import preprocessing
 from VidDataLoader import VidDataset
+from VidDataLoaderLight import VidDatasetLight
 
 
 def parse_args():
@@ -15,18 +16,20 @@ def parse_args():
         description='Data cleaner for deepcluster. It deletes not annotated images in a vast dataset.')
     parser.add_argument('data', metavar='DATA_DIR', type=str, help='Dir where dataset images are saved.')
     parser.add_argument('ann', metavar='ANN_DIR', type=str, help='Dir where images annotations are saved.')
+    parser.add_argument('labels_txt', metavar='LBL_TXT', type=str,
+                        help='txt file where label where labels are provided along with objects names.')
     parser.add_argument('--data_type', metavar='DATA_TYPE', type=str, default='train',
                         help='Data type to be cleaned; it must match data dir name. For example: train, val. Default: train.')
     parser.add_argument('--output', metavar='LOG', type=str, default='', help='Dir where useful logs will be saved.')
     return parser.parse_args()
 
 
-def serialize(dataset, tag='vid_dataset'):
+def serialize(dataset, tag=None):
     print('Serializing cleaned dataset...')
     now = datetime.datetime.now()
     date_str = f'{now.year}.{now.month}.{now.day}_{now.hour}_{now.minute}_{now.second}'
     filename = f'{tag}_{date_str}'
-    with open(args.output+filename + '.pkl', 'wb') as f:
+    with open(args.output + filename + '.pkl', 'wb') as f:
         pickle.dump(dataset, f)
 
 
@@ -36,17 +39,19 @@ def parse_annotation(ann):
         img (string): the image to which coordinates are related
         ann: an xml file with bb annotations: xmin, xmax, ymin, ymax
     Returns:
-        a list with bb coordinates for a given image.
+        a dictionary with bb coordinates and category for a given image.
     Exception:
         if coordinates are not present in xml file.
     """
     try:
         xml = minidom.parse(ann)
-        int(xml.getElementsByTagName('xmin')[0].firstChild.data), \
-        int(xml.getElementsByTagName('xmax')[0].firstChild.data), \
-        int(xml.getElementsByTagName('ymin')[0].firstChild.data), \
-        int(xml.getElementsByTagName('ymax')[0].firstChild.data)
-        return True
+        crop_coord = {'xminx': int(xml.getElementsByTagName('xmin')[0].firstChild.data),
+                      'xmax': int(xml.getElementsByTagName('xmax')[0].firstChild.data),
+                      'ymin': int(xml.getElementsByTagName('ymin')[0].firstChild.data),
+                      'ymax': int(xml.getElementsByTagName('ymax')[0].firstChild.data)
+                      }
+        label = xml.getElementsByTagName('name')[0].firstChild.data
+        return crop_coord, label
 
     except Exception:
         return False
@@ -54,25 +59,38 @@ def parse_annotation(ann):
 
 def main(args):
     print('Load dataset...')
-    logging.basicConfig(filename=args.output + 'clean.log', filemode='w', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=args.output + 'clean.log', filemode='w', level=logging.INFO,
+                        format='%(name)s - %(levelname)s - %(message)s')
 
+    # Prepare dataset
     tra = [preprocessing.Rescale((224, 224)),
            preprocessing.ToTensor()]
-    dataset = VidDataset(xml_annotations_dir=args.ann, root_dir=args.data, transform=transforms.Compose(tra))
+    dataset = VidDatasetLight(root_dir=args.data, transform=transforms.Compose(tra))
+
+    # Prepare labels
+    labels = {}
+    with open(args.labels_txt, 'r') as f:
+        for line in f:
+            lbl = line.rstrip().split(' ')
+            labels[lbl[0]] = {'code': lbl[1], 'class': lbl[2]}
+
     print('Dataset loaded.\n')
     print('Clean dataset...\n')
     not_annotated_imgs_idx = []
     for idx, img in enumerate(dataset.imgs):
         ann = (args.ann + img[0].split('/' + args.data_type + '/')[1]).split('.')[0] + '.xml'
-        if not parse_annotation(ann):
+        crop_coord, label = parse_annotation(ann)
+        if False in (crop_coord, label):
             not_annotated_imgs_idx.append(idx)
             logging.info("Removed " + img[0] + "\n")
-        if not (idx+1) % 1000:
-            print("Analyzed ", idx+1, "images")
+        else:
+            pass
+        if not (idx + 1) % 1000:
+            print("Analyzed ", idx + 1, "images")
     for i in sorted(not_annotated_imgs_idx, reverse=True):
         del dataset.imgs[i]
     logging.info("Removed " + str(len(not_annotated_imgs_idx)) + " images.\n")
-    serialize(dataset)
+    serialize(dataset, tag='vid_dataset' + args.data_type)
     print('Task terminated. Find log and pkl file at ' + args.output)
 
 
--- clean_data.sh
+++ clean_data.sh
@@ -5,13 +5,14 @@
 # LICENSE file in the root directory of this source tree.
 #
 #!/bin/bash
-DATA_TYPE="val"
+DATA_TYPE="train"
 DATA_DIR="/mnt/ILSVRC2017_VID/ILSVRC/Data/VID/${DATA_TYPE}/"
 ANN_DIR="/mnt/ILSVRC2017_VID/ILSVRC/Annotations/VID/${DATA_TYPE}/"
+LBL_TXT="/home/${USER}/video_deepcluster/map_vid.txt"
 EXP="/thecube/students/${USER}/"
 
 PYTHON="/home/${USER}/miniconda/envs/vmr/bin/python"
 
-${PYTHON} data-cleaner.py ${DATA_DIR} ${ANN_DIR} --output ${EXP} --data_type ${DATA_TYPE}
+${PYTHON} data-cleaner.py ${DATA_DIR} ${ANN_DIR} ${LBL_TXT} --output ${EXP} --data_type ${DATA_TYPE}
 #/miniconda/envs/vmr/bin/python
 
